bdddc322baa231f15d2f28a099a3b4af0add9b15
Imron <imron.gamidli@lt.hs-fulda.de>
2022-05-15 18:23:41 +0200

    feat: use RecyclerView in MainActivity

--- app/src/main/java/com/example/words/MainActivity.kt
+++ app/src/main/java/com/example/words/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.words
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
+import com.example.words.adapter.LetterAdapter
+import com.example.words.data.Datasource
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    val myDataset = Datasource().loadLetters()
+    val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+    recyclerView.adapter = LetterAdapter(this, myDataset)
+    recyclerView.setHasFixedSize(true)
   }
 }
