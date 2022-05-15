882e78204d36c5f05f74da676882ad5143a5c669
Imron <imron.gamidli@lt.hs-fulda.de>
2022-05-15 17:51:15 +0200

    feat: add adapter for recycler view

---
+++ app/src/main/java/com/example/words/adapter/LetterAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.words.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import com.example.words.R
+
+class LetterAdapter(
+  private val context: Context,
+  private val dataset: List<Char>
+) : RecyclerView.Adapter<LetterAdapter.LetterViewHolder>() {
+
+  class LetterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val button: Button = view.findViewById(R.id.letter_button)
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LetterViewHolder {
+    val adapterLayout = LayoutInflater.from(parent.context)
+      .inflate(R.layout.letter_item, parent, false)
+    return LetterViewHolder(adapterLayout)
+  }
+
+  override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
+    val item = dataset[position]
+    holder.button.text = item.toString()
+  }
+
+  override fun getItemCount(): Int = dataset.size
+}
